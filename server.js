bc29d9acc60655dcc53c788522a1b045989f0d3e
Mukund Madhav <mukundmadhav054@gmail.com>
2024-01-25 21:42:57 +0530

    updated server.js with error handling and env for port number

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.route("/ping").get((req, res) => {
-  res.send("pong");
+const errorHandler = (err, req, res, next) => {
+  console.log(err);
+  res.status(500).send(err.message);
+};
+
+app.route("/ping").get((req, res, next) => {
+  try {
+    res.send("pong");
+  } catch (err) {
+    next(err);
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`App is running on PORT: ${port}`);
 });
 
-module.exports = app
+module.exports = app;
