f28f78b6c332f7d6803d245b2394be06a4383027
mukundmadhav054 <mukundmadhav054@gmail.com>
2024-02-11 22:50:20 +0530

    update from feedback

--- client/src/components/Superstition.jsx
+++ client/src/components/Superstition.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Superstition = () => {
   return (
     <div className="flex flex-col items-center m-8">
-      <div className="card flex flex-col border-2 border-black rounded-2xl drop-shadow-2xl px-8 py-4">
+      <div className="card flex flex-col border-2 border-black rounded-2xl drop-shadow-2xl py-4 lg:px-8 md:px-4 sm:px-2">
         <h2 className="title font-bold text-2xl">Black Cat Crossings</h2>
         <p className="description italic">
           Crossing paths with a black cat is associated with bad luck, believed
