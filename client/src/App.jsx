d15cbb6e69b9f03d26c57ca4c3475f0e6469591c
mukundmadhav054 <mukundmadhav054@gmail.com>
2024-02-11 17:54:27 +0530

    landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="flex justify-between px-10 py-4">
+        <div className="flex justify-evenly w-2/4">
+          <a
+            href="#"
+            className="text-[#5b21b6] text-xl font-semibold tracking-wider hover:font-bold hover:text-red-600"
+          >
+            ODD SUPERSTITIONS
+          </a>
+          <a href="#" className="text-lg">
+            Home
+          </a>
+          <a href="#" className="text-lg">
+            About
+          </a>
+          <a href="#" className="text-lg">
+            FAQ
+          </a>
+          <a href="#" className="text-lg">
+            Contact Us
+          </a>
+        </div>
+        <div className="flex justify-evenly w-1/6">
+          <button className="border-2 border-black rounded px-4 py-1 min-h-full hover:bg-black hover:text-white">
+            Login
+          </button>
+          <button className="border-2 border-black rounded px-4 py-1 hover:bg-black hover:text-white">
+            Sign Up
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
