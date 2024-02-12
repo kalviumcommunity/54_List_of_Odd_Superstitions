d5ff541576b846769a9f769deb33f280b64b19d3
mukundmadhav054 <mukundmadhav054@gmail.com>
2024-02-12 23:30:26 +0530

    using component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [showNavLinks, setShowNavLinks] = useState(false);
+
+  const toggleNavLinks = () => {
+    setShowNavLinks(!showNavLinks);
+  };
+
   return (
     <header>
-      <nav className="flex justify-between px-10 py-4">
-        <div className="flex justify-evenly w-2/4">
+      <nav className="flex h-[10vh]">
+        <div className="flex justify-between items-center w-full md:w-full px-4">
           <a
             href="javascript:void(0);"
             className="text-[#5b21b6] text-xl font-semibold tracking-wider hover:font-bold hover:text-red-600"
           >
             ODD SUPERSTITIONS
           </a>
-          <a href="javascript:void(0);" className="text-lg">
+          <button className="lg:hidden px-2 py-1" onClick={toggleNavLinks}>
+            ☰
+          </button>
+        </div>
+        <div
+          className={`lg:flex md:w-full justify-evenly items-center ${
+            showNavLinks ? "block" : "hidden"
+          }`}
+        >
+          <a href="javascript:void(0);" className="md:text-lg">
             Home
           </a>
-          <a href="javascript:void(0);" className="text-lg">
+          <a href="javascript:void(0);" className="md:text-lg">
             About
           </a>
-          <a href="javascript:void(0);" className="text-lg">
+          <a href="javascript:void(0);" className="md:text-lg">
             FAQ
           </a>
-          <a href="javascript:void(0);" className="text-lg">
+          <a href="javascript:void(0);" className="md:text-lg">
             Contact Us
           </a>
-        </div>
-        <div className="flex justify-evenly w-1/6">
-          <button className="border-2 border-black rounded px-4 py-1 min-h-full hover:bg-black hover:text-white">
+          <button className="border-2 border-black rounded px-4 py-1 hover:bg-black hover:text-white h-fit">
             Login
           </button>
-          <button className="border-2 border-black rounded px-4 py-1 hover:bg-black hover:text-white">
+          <button className="border-2 border-black rounded px-4 py-1 hover:bg-black hover:text-white h-fit">
             Sign Up
           </button>
         </div>
--- client/src/components/Superstition.jsx
+++ client/src/components/Superstition.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Superstition = () => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          "https://odd-superstitions.onrender.com/superstition"
+        );
+        setData(res.data);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="flex flex-col items-center m-8">
-      <div className="card flex flex-col border-2 border-black rounded-2xl drop-shadow-2xl py-4 lg:px-8 md:px-4 sm:px-2">
-        <h2 className="title font-bold text-2xl">Black Cat Crossings</h2>
-        <p className="description italic">
-          Crossing paths with a black cat is associated with bad luck, believed
-          to be an omen of misfortune or linked to dark magic.
-        </p>
-      </div>
-      <hr />
-    </div>
+    <>
+      {error ? (
+        <div className="flex justify-center items-center">
+          <p>Error: {error}</p>
+        </div>
+      ) : (
+        data.map((e) => (
+          <div
+            key={e._id}
+            className="flex flex-col items-center mx-4 my-8 lg:m-8"
+          >
+            <div className="card flex flex-col border-2 border-black rounded-2xl drop-shadow-2xl py-4 lg:px-8 md:px-4 px-3 md:w-3/4 ">
+              <h2 className="title font-bold text-2xl">{e.title}</h2>
+              <p className="description italic text-justify">{e.description}</p>
+            </div>
+            <hr />
+          </div>
+        ))
+      )}
+    </>
   );
 };
 
