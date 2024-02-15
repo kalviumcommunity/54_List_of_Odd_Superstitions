1e9cbb30a52a43ebbeaf63d540207f0b733047dd
mukundmadhav054 <mukundmadhav054@gmail.com>
2024-02-15 16:19:35 +0530

    updated files from feedback and some minor changes

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -15,12 +15,13 @@ const Form = () => {
     handleSubmit,
     watch,
     reset,
-    formState: { errors, isSubmitSuccessful, isSubmitted },
+    formState: { errors, isSubmitSuccessful, isSubmitted, isSubmitting },
   } = useForm();
 
   const submitHandler = async (formData) => {
     setData(formData);
     try {
+      setIsLoading(true);
       const res = await axios.post(
         "https://odd-superstitions.onrender.com/superstition",
         formData,
@@ -46,6 +47,8 @@ const Form = () => {
       reset();
     } catch (error) {
       errorHandler(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -66,78 +69,81 @@ const Form = () => {
   return (
     <div className="flex justify-center items-center h-[75vh] form-container">
       {isLoading && <RotateLoader color="#000000" />}
-      <form
-        onSubmit={handleSubmit(submitHandler)}
-        className="flex flex-col justify-center items-center border-2 rounded-xl border-gray-500 p-[3%] lg:px-[1%] lg:py-[2%]"
-      >
-        <div className="my-2">
-          <div className="group relative w-72 md:w-80 lg:w-96">
-            <label
-              htmlFor="1"
-              className="block w-full pb-1 text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
-            >
-              Title
-            </label>
-            <input
-              id="1"
-              type="text"
-              className="peer h-10 w-full rounded-md bg-gray-50 px-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400"
-              name="title"
-              {...register("title", {
-                required: "Title is required",
-                minLength: {
-                  value: 3,
-                  message: "Title cannot be less than 3 characters.",
-                },
-              })}
-            />
+      {!isLoading && (
+        <form
+          onSubmit={handleSubmit(submitHandler)}
+          className="flex flex-col justify-center items-center border-2 rounded-xl border-gray-500 p-[3%] lg:px-[1%] lg:py-[2%]"
+        >
+          <div className="my-2">
+            <div className="group relative w-72 md:w-80 lg:w-96">
+              <label
+                htmlFor="1"
+                className="block w-full pb-1 text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
+              >
+                Title
+              </label>
+              <input
+                id="1"
+                type="text"
+                className="peer h-10 w-full rounded-md bg-gray-50 px-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400"
+                name="title"
+                {...register("title", {
+                  required: "Title is required",
+                  minLength: {
+                    value: 3,
+                    message: "Title cannot be less than 3 characters.",
+                  },
+                })}
+              />
+            </div>
+            <p className="text-red-600 font-medium text-sm">
+              {errors.title?.message}
+            </p>
           </div>
-          <p className="text-red-600 font-medium text-sm">
-            {errors.title?.message}
-          </p>
-        </div>
 
-        <div className="my-2">
-          <div className="group relative w-72 md:w-80 lg:w-96">
-            <label
-              htmlFor="2"
-              className="block w-full pb-1 text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
-            >
-              Description
-            </label>
-            <input
-              id="2"
-              type="text"
-              className="peer h-10 w-full rounded-md bg-gray-50 px-4 font-thin outline-none drop-shadow-sm transition-all duration-200 ease-in-out focus:bg-white focus:ring-2 focus:ring-blue-400"
-              name="description"
-              {...register("description", {
-                required: "Description is required",
-                minLength: {
-                  value: 5,
-                  message: "Description cannot be less than 5 characters.",
-                },
-              })}
-            />
+          <div className="my-2">
+            <div className="group relative w-72 md:w-80 lg:w-96">
+              <label
+                htmlFor="2"
+                className="block w-full pb-1 text-sm font-medium text-gray-500 transition-all duration-200 ease-in-out group-focus-within:text-blue-400"
+              >
+                Description
+              </label>
+              <textarea
+                id="2"
+                type="text"
+                rows={4}
+                className="flex min-h-[60px] w-full rounded-md border border-input px-3 py-2 text-sm shadow-sm focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                name="description"
+                {...register("description", {
+                  required: "Description is required",
+                  minLength: {
+                    value: 5,
+                    message: "Description cannot be less than 5 characters.",
+                  },
+                })}
+              ></textarea>
+            </div>
+            <p className="text-red-600 font-medium text-sm">
+              {errors.description?.message}
+            </p>
           </div>
-          <p className="text-red-600 font-medium text-sm">
-            {errors.description?.message}
-          </p>
-        </div>
 
-        <div className=" mt-1 flex justify-around w-full">
-          <input
-            type="submit"
-            value={"Add Superstition"}
-            className="border-2 hover:bg-green-500 hover:text-white border-green-500 px-4 py-2"
-          />
-          <button
-            onClick={() => reset()}
-            className="border-2 hover:bg-red-500 hover:text-white border-red-500 px-4 py-2"
-          >
-            RESET
-          </button>
-        </div>
-      </form>
+          <div className=" mt-1 flex justify-around w-full">
+            <input
+              type="submit"
+              value={"Add Superstition"}
+              className="border-2 hover:bg-green-500 hover:text-white border-green-500 px-4 py-2"
+            />
+            <button
+              onClick={() => reset()}
+              className="border-2 hover:bg-red-500 hover:text-white border-red-500 px-4 py-2"
+            >
+              RESET
+            </button>
+          </div>
+        </form>
+      )}
       <ToastContainer />
     </div>
   );
